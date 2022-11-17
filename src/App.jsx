a420382827c330d90838ca8d379783794fc30167
Gabriela Florêncio <perflogabi@gmail.com>
2022-11-17 17:13:24 -0300

    quarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
 import './App.css';
 import { Welcome } from './components/Welcome';
 
 function App() {
+
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div className="App">
-      <h1>Quiz app</h1>
+      <h1>Quiz de Programação</h1>
       <Welcome/>
     </div>
   );
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,14 +1,22 @@
+import { useContext } from 'react'
+import { QuizContext } from '../context/quiz';
+
 import React from 'react'
-import Quiz from "../img/img1w.png"
+import Quiz from "../img/imgw.png"
 import"./Welcome.css";
 
-export const Welcome = () => {
+const Welcome = () => {
+
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div id='welcome'>
         <h2>Seja bem-vindo</h2>
         <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
+        <button onClick={() => dispatch({ type: "CHANGE_STATE"})}>Iniciar</button>
         <img src={Quiz} alt="Início do quiz"></img>
     </div>
   )
 }
+
+export default Welcome;
